682a632e2e3dcf543465dd49d09b7f69e7450253
Dung Nguyen <bluesky.1289@gmail.com>
2019-11-05 23:30:42 +0700

    add highlight js

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,22 +1,9 @@
 import "../scss/index.scss";
 
 import * as $ from "jquery";
+import * as hljs from "highlight.js";
 
-// Floating label headings for the contact form
-$("body")
-  .on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass(
-      "floating-label-form-group-with-value",
-      !!$(e.target).val()
-    );
-  })
-  .on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  })
-  .on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
-  });
-
+hljs.initHighlightingOnLoad();
 // Show the navbar when the page is scrolled up
 var MQL = 992;
 
--- config/dev.exs
+++ config/dev.exs
@@ -83,6 +83,13 @@ config :chubi,
     intro:
       "Welcome to my personal blog. My name is Wangchuk, I am a full-stack web developer. That's it.",
     ga: "my code",
+    author: %{
+      fullname: "Dzung Nguyen",
+      email: "bluesky.1289@gmail.com",
+      avatar: "https://i.pravatar.cc/300",
+      bio:
+        "Hello! I am Dzung, I like blogging about interesting things. I don't talk much. When talking I don't have time to observe and think a bout the world."
+    },
     menu: [
       main: [
         %{
