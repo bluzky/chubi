9cea4793084d1818880ed05255094b3b8bade31b
Dung Nguyen <bluesky.1289@gmail.com>
2019-11-11 21:36:03 +0700

    clean assets

--- lib/chubi_web/views/partial_view.ex
+++ lib/chubi_web/views/partial_view.ex
@@ -1,3 +1,23 @@
 defmodule ChubiWeb.PartialView do
   use ChubiWeb, :view
+
+  require Ecto.Query
+  alias Chubi.Repo
+  alias Chubi.PostMeta.PostMetaQuery
+
+  def list_top_categories(limit \\ 5) do
+    PostMetaQuery.category_query()
+    |> PostMetaQuery.category_with_post_count()
+    |> PostMetaQuery.order_by_post_count()
+    |> Ecto.Query.limit(^limit)
+    |> Repo.all()
+  end
+
+  def list_top_tags(limit \\ 5) do
+    PostMetaQuery.tag_query()
+    |> PostMetaQuery.tag_with_post_count()
+    |> PostMetaQuery.order_by_post_count()
+    |> Ecto.Query.limit(^limit)
+    |> Repo.all()
+  end
 end
