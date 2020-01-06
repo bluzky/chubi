61ebc7e1c15045fb948a4d4c1cee39b63ec5cd64
Dung Nguyen <bluesky.1289@gmail.com>
2020-01-06 22:25:39 +0700

    update migration to fix duplicated index

--- priv/repo/migrations/20191203142501_update_meta_unique_constraints.exs
+++ priv/repo/migrations/20191203142501_update_meta_unique_constraints.exs
@@ -2,13 +2,11 @@ defmodule Chubi.Repo.Migrations.UpdateMetaUniqueConstraints do
   use Ecto.Migration
 
   def up do
-    create unique_index(:posts, [:slug])
     create unique_index(:categories, [:slug])
     create unique_index(:tags, [:slug])
   end
 
   def down do
-    drop_if_exists unique_index(:posts, [:slug])
     drop_if_exists unique_index(:categories, [:slug])
     drop_if_exists unique_index(:tags, [:slug])
   end
